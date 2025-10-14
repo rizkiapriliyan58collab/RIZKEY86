3bfc833ceec89b84b679467d1913046ac9f103fb
rizkiapriliyan58collab <rizkiapriliyan58@gmail.com>
2025-10-14 11:44:51 +0700

    Update and rename srip.js to sript.js

---
+++ sript.js
@@ -0,0 +1,7 @@
+//1.Pilih tombol berdasarkan ID-nya
+cont tombolsapa = document.getElementByld('sapaButton');
+//2.Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click',function() {
+  //3.Jalankan kode ini ketika tombol di-klik
+  alert(/halo terimakasih sudah berkunjung!);
+));
